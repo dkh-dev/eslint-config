296cc624afaf670895b53a9f6f87ecd55ef7c529
Hoang <dangkyokhoang@gmail.com>
2021-05-19 21:56:35 +0700

    Source type: module

--- test/index.js
+++ test/index.js
@@ -1,17 +1,12 @@
 'use strict'
 
-const { CLIEngine } = require('eslint')
+const { ESLint } = require('eslint')
 const test = require('tape')
 
-test('validate syntax', t => {
-    const code = `'use strict'\n\nconst foo = 1\nconst bar = () => void 0\n\nbar(foo)\n`
-    const cli = new CLIEngine({
-        useEslintrc: false,
-        configFile: 'index.js',
-    })
+test('validate eslint config', async t => {
+  const eslint = new ESLint()
+  const [ { errorCount, warningCount } ] = await eslint.lintFiles(__filename)
 
-    const { errorCount, warningCount } = cli.executeOnText(code)
-
-    t.equal(errorCount + warningCount, 0)
-    t.end()
+  t.equal(errorCount + warningCount, 0)
+  t.end()
 })
